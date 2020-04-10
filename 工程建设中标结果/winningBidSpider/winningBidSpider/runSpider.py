eea3d684741fd8f40e8954d882538bc4cc060b51
506780892@qq.com <506780892@qq.com>
2020-04-10 18:08:44 +0800

    scr

---
+++ 工程建设中标结果/winningBidSpider/winningBidSpider/runSpider.py
@@ -0,0 +1,10 @@
+"""
+    @author: Tomc
+    @contact: www.506780892@qq.com
+    @software: PyCharm
+    @file: cqggzyRule.py
+    @time: 2019/11/28 19:12
+    @desc:
+"""
+from scrapy import cmdline
+cmdline.execute('scrapy crawl gdggzy'.split())
